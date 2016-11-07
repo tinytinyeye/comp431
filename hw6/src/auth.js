f531420f0b33a3cf95c511f867e14eba0c7b25d6
tinytinyeye <frankiewong@HuangQixuans-MacBook-Pro.local>
2016-11-07 14:44:11 -0600

    hw6-backend-auth

--- hw6/src/auth.js
+++ hw6/src/auth.js
@@ -1,40 +1,15 @@
 'use strict'
 
-const md5 = require('md5')
-
-const database = {}
-
-const sessionUser = {}
-
-const cookieKey = 'abc'
+const username = 'qh5'
 
 const register = (req, res) => {
-     console.log('Payload received', req.body)
-     let payload = req.body
-     let username = payload.username
-     let password = payload.password
-     let salt = Math.random()
-     let hash = md5(password + salt)
-     database[username] = { salt, hash }
-     res.send({ username : username, status : 'success' })
+	let user = req.body.username ? req.body.username : username
+    res.send({ result: 'success', username: user })
 }
 
 const login = (req, res) => {
-     console.log('Payload received', req.body)
-     let payload = req.body
-     let username = payload.username
-     let password = payload.password
-     let salt = database[username].salt
-     let hash = database[username].hash
-     if (md5(password + salt) === hash) {
-          res.cookie(cookieKey, 12345, 
-               {maxAfe: 3600*1000, httpOnly: true })
-          sessionUser[12345] = username
-          const msg = { username : username, status: 'success'}
-          res.send(msg)
-     } else {
-          res.sendStatus(401)
-     }
+	let user = req.body.username ? req.body.username : username
+     res.send({ result: "success", username : user })
 }
 
 const logout = (req, res) => {
@@ -42,7 +17,7 @@ const logout = (req, res) => {
 }
 
 const changePassword = (req, res) => {
-     res.send({ username : 'qh5', status : 'will not change' })
+     res.send({ username : username, status : 'will not change' })
 }
 
 module.exports = app => {
