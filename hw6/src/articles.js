68e89aa87d3a612bdf02722f692a8f944be0c329
tinytinyeye <frankiewong@HuangQixuans-MacBook-Pro.local>
2016-11-09 00:43:07 -0600

    _id

--- hw6/src/articles.js
+++ hw6/src/articles.js
@@ -1,16 +1,16 @@
 'use strict'
 
 let articles = [
-		{ "id" : 1, "author" : "John", "text" : "My first article",
+		{ "_id" : 1, "author" : "John", "text" : "My first article",
            "date" : "2016-11-06T02:41:58.128Z", "img" : "", "comment" : [] },
-		{ "id" : 2, "author" : "Ben", "text" : "My second article",
+		{ "_id" : 2, "author" : "Ben", "text" : "My second article",
            "date" : "2016-11-06T02:41:58.128Z", "img" : "", "comment" : [] },
-		{ "id" : 3, "author" : "Wong", "text" : "My third article",
+		{ "_id" : 3, "author" : "Wong", "text" : "My third article",
            "date" : "2016-11-06T02:41:58.128Z", "img" : "", "comment" : [] }
 		]
 
 const addArticle = (req, res) => {
-     let newArticle = { "id" : articles.length + 1, "author" : "qh5", 
+     let newArticle = { "_id" : articles.length + 1, "author" : "qh5", 
                "text" : req.body["text"], "date" : new Date(), 
                "img" : "", "comment" : []}
      articles[articles.length] = newArticle
@@ -19,7 +19,7 @@ const addArticle = (req, res) => {
 
 const editArticle = (req, res) => {
      let id = req.params.id
-     newArticle = { "id" : id, "author" : 'Bin', 
+     newArticle = { "_id" : id, "author" : 'Bin', 
           "text" : "alsjdhkajshdkasjhd", "date" : "2016-11-06T02:41:58.128Z",
            "img" : "a.jpg", "comment" : []}
      res.send({"articles" : [newArticle]})
